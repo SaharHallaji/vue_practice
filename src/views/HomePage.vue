714142a8454e7174ba64b1b98c4157d2614848a1
Sahar Hallaji <saharhallaji.dev@gmail.com>
2023-03-08 18:29:30 +0330

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+import type {Ref} from "vue";
+interface book {
+  title: Ref<string>,
+  pages?: number
+}
+const books: book = { title:ref("hi")}
+
+</script>
+
+<template>
+  <main>
+    home
+    <p>
+      {{ books.title + "sahar" }}
+    </p>
+  </main>
+</template>
